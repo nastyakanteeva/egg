2edc3d6dfd797cb87c0943884fa2b6a3f3b43d66
nastyakanteeva <117898518+nastyakanteeva@users.noreply.github.com>
2023-09-20 21:18:38 +0300

    Add files via upload

---
+++ egg/egg/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    static void MyThreadChicken()
+    {
+        for (int i = 0; i < 100; i++)
+            Console.WriteLine("Яйцо");
+    }
+    private static void Main(string[] args)
+    {
+        Thread chicken = new(MyThreadChicken);
+        chicken.Start();
+        for (int i = 0; i < 100; i++)
+            Console.WriteLine("Курица");
+        MyThreadAnswer(chicken);
+    }
+    static void MyThreadAnswer(Thread chicken)
+    {
+
+        bool ans = chicken.IsAlive;
+        Console.WriteLine(ans ? "Спор Окончен. Победило Яйцо." : "Спор окончен. Победила Курица.");
+    }
+}
